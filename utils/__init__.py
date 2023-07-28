0a33d24806c7c64167373e3fa4529488493eb64d
Delsus78 <delsustheugm@gmail.com>
2023-07-28 16:25:26 +0200

    Initial commit

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+import os
+
+from utils import log
+
+log.setup()
+
+# On Windows, the selector event loop is required for aiodns.
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
